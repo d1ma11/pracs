a351ff8d81d3e0d80de9774d977e4b147a3311c0
Дмитрий Данилов <danilov0330@gmail.com>
2022-12-05 03:02:07 +0300

    4rd commit

--- src/prac20/task4/MinMax.java
+++ src/prac20/task4/MinMax.java
@@ -1,55 +1,62 @@
 package prac20.task4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
-public class MinMax<T extends Number> implements Comparator<T> {
+public class MinMax<T extends Comparable<T>> {
+    private T[] arr;
 
-    List<T> array;
-
-    MinMax(List<T> arr) {
-        array = arr;
+    public MinMax(T[] arr) {
+        this.arr = arr;
     }
 
-    public static Number findMax(List<Number> arr) {
-        return 0;
+    public T getMinElement() {
+        if (arr.length > 0) {
+            T min = arr[0];
+            for (T t : arr) {
+                if (t.compareTo(min) < 0) {
+                    min = t;
+                }
+            }
+            return min;
+        }
+        return null;
     }
 
-    @Override
-    public int compare(Number o1, Number o2) {
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        MinMax<Number> list = new MinMax<>(
-                Arrays.asList(4,5,6,6,5,3.745,456.7,325f)
-        );
-
-        System.out.println(findMax(list.array));
+    public T getMaxElement() {
+        if (arr.length > 0) {
+            T max = arr[0];
+            for (T t : arr) {
+                if (t.compareTo(max) > 0) {
+                    max = t;
+                }
+            }
+            return max;
+        }
+        return null;
     }
 }
 
-//    List<T> array;
-//
-//    MinMax(List<T> arr) {
-//        array = arr;
-//    }
-//
-//    public static Number findMax(List<Number> arr) {
-//        Number max = null;
-//        for (int i = 0; i < arr.size()-1; i++) {
-//            if(arr.get(i) > max)
-//                max = arr.get(i);
-//        }
-//        return max;
-//    }
-//
-//    public static void main(String[] args) {
-//        MinMax<Number> list = new MinMax<>(
-//                Arrays.asList(4,5,6,6,5,3.745,456.7,325f)
-//        );
-//
-//        System.out.println(findMax(list.array));
-//    }
+class Calculator {
+    public static double sum(Number num1, Number num2) {
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public static double multiply(Number num1, Number num2) {
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public static double divide(Number num1, Number num2) {
+        return num1.doubleValue() / num2.doubleValue();
+    }
+
+    public static double subtraction(Number num1, Number num2) {
+        return num1.doubleValue() - num2.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(3.5f, 6L));
+        System.out.println(multiply(1, 0.5));
+        System.out.println(divide(15.0, 3f));
+        System.out.println(subtraction(228.1337, 666));
+    }
+}
